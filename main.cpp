946c48f31664c989a32cd85c678fb78d14cabacd
pogacee <pogacee@curtisinst.com>
2023-03-25 04:40:41 -0700

    changed test to match new api

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
-#include "moveXY.h"
+#include "MoveXY.h"
 #include <iostream>
 
 int main()
 {
     std::cout << "hello world" << std::endl;
-    moveXY foo(20.0f, 30.0f, 22.5f, 34.3f);
 
-    moveXY::Point target{4, 5};
-    moveXY::Point p1{0, 0};
-    moveXY::Point p2{0, 0};
-    int x = foo.getCircleInts(5, 6, target, p1, p2);
+    constexpr double armLen{22.5};
+    constexpr double wristLen{33.4f};
+    MoveXY foo(0, 0, armLen, wristLen);
+
+    MoveXY::Point target{-wristLen, -armLen};
+    int x = foo.getCircleInts(target);
 
     std::cout << "Num Sol: " << x << std::endl;
-    std::cout << p1.x << ", " << p1.y << std::endl;
-    std::cout << p2.x << ", " << p2.y << std::endl;
+    std::cout << foo.get_theta_shoulder(foo.i1) << ", " << foo.i1.x << " : " << foo.i1.y << std::endl;
+    std::cout << foo.get_theta_shoulder(foo.i2) << ", " << foo.i2.x << " : " << foo.i2.y << std::endl;
 }
