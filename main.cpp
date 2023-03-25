ac2688faeb6f9f4136f85a0c7b9e5bef23fba871
pogacee <pogacee@curtisinst.com>
2023-03-25 02:52:44 -0700

    make readable

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::cout << "hello world";
+    std::cout << "hello world" << std::endl;
 
     moveXY foo = moveXY(0, 0, 0, 0);
     moveXY::Point target{4, 5};
@@ -11,7 +11,7 @@ int main()
     moveXY::Point p2{0, 0};
     int x = foo.getCircleInts(5, 6, target, p1, p2);
 
-    std::cout << "Num Sol" << x << std::endl;
-    std::cout << p1.x << ", " << p1.y;
-    std::cout << p2.x << ", " << p2.y;
+    std::cout << "Num Sol: " << x << std::endl;
+    std::cout << p1.x << ", " << p1.y << std::endl;
+    std::cout << p2.x << ", " << p2.y << std::endl;
 }
