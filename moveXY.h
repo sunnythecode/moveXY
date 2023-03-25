08b819b53a88686f704f9fc53b13eb96f6ad2b1d
pogacee <pogacee@curtisinst.com>
2023-03-25 04:40:16 -0700

    moved more towards obj oriented style

--- moveXY.h
+++ moveXY.h
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cmath>
 
-class moveXY
+class MoveXY
 {
 public:
     class Point
@@ -12,14 +12,23 @@ public:
         double x;
         double y;
 
-        double norm()
+        double magnitude()
         {
             return std::sqrt(x * x + y * y);
         }
+        double get_angle()
+        {
+            return atan2(y, x);
+        }
+
         void setCoords(double in_x, double in_y)
         {
             x = in_x;
             y = in_y;
+        }
+        static double to_degrees(double radians)
+        {
+            return radians * 180 / 3.1415;
         }
     };
 
@@ -29,14 +38,13 @@ public:
         double elbow;
     };
 
-    Point m_start_xy,
-        m_curr_xy;
+    Point m_start_xy, m_curr_xy;
     ArmSolution m_offset_theta;
     ArmSolution m_curr_theta;
-    float m_elbow_len,
-        m_shoulder_len;
+    float m_elbow_len, m_shoulder_len;
+    Point i1, i2;
 
-    getQuadrant(Point pt)
+    int getQuadrant(Point pt)
     {
         int res = -1;
         if (pt.x >= 0)
@@ -69,28 +77,29 @@ public:
         return outputPoints;
     }
 
-    size_t getCircleInts(float originRadius, float targetRadius, Point target, Point &i1, Point &i2)
+    size_t getCircleInts(Point target)
     {
-        double d = Point{0 - target.x, 0 - target.y}.norm();
+
+        double d = Point{0 - target.x, 0 - target.y}.magnitude();
 
         // find number of solutions
-        if (d > originRadius + targetRadius) // circles are too far apart, no solution(s)
+        if (d > m_shoulder_len + m_elbow_len) // circles are too far apart, no solution(s)
         {
             return 0;
         }
-        else if (d == 0 && originRadius == targetRadius) // circles coincide
+        else if (d == 0 && m_shoulder_len == m_elbow_len) // circles coincide
         {
             return 0;
         }
         // one circle contains the other
-        else if (d + std::min(originRadius, targetRadius) < std::max(originRadius, targetRadius))
+        else if (d + std::min(m_shoulder_len, m_elbow_len) < std::max(m_shoulder_len, m_elbow_len))
         {
             return 0;
         }
         else
         {
-            double a = (originRadius * originRadius - targetRadius * targetRadius + d * d) / (2.0 * d);
-            double h = std::sqrt(originRadius * originRadius - a * a);
+            double a = (m_shoulder_len * m_shoulder_len - m_elbow_len * m_elbow_len + d * d) / (2.0 * d);
+            double h = std::sqrt(m_shoulder_len * m_shoulder_len - a * a);
 
             // find p2
             Point p2{0 + (a * (target.x - 0)) / d, 0 + (a * (target.y - 0)) / d};
@@ -99,15 +108,15 @@ public:
             i1.setCoords(p2.x + (h * (target.y - 0) / d), p2.y - (h * (target.x - 0) / d));
             i2.setCoords(p2.x - (h * (target.y - 0) / d), p2.y + (h * (target.x - 0) / d));
 
-            if (d == originRadius + targetRadius)
+            if (d == m_shoulder_len + m_elbow_len)
                 return 1;
             return 2;
         }
     }
 
-    double distanceToElbow(Point currPos, Point targetPos)
+    double distanceToElbow(Point targetPos)
     {
-        return sqrt(((currPos.x - targetPos.x) * (currPos.x - targetPos.x)) + ((currPos.y - targetPos.y) * (currPos.y - targetPos.y)));
+        return sqrt(((m_curr_xy.x - targetPos.x) * (m_curr_xy.x - targetPos.x)) + ((m_curr_xy.y - targetPos.y) * (m_curr_xy.y - targetPos.y)));
     }
 
     double convertToAdjustedCoordinateSystem(double offset, double start_angle_offset)
@@ -120,7 +129,7 @@ public:
         return adjustedAngle + start_angle_offset - 360;
     }
 
-    moveXY(float start_angle_offset_shoulder, float start_angle_offset_elbow, float shoulder_len, float elbow_len)
+    MoveXY(float start_angle_offset_shoulder, float start_angle_offset_elbow, float shoulder_len, float elbow_len)
     {
         m_curr_theta.elbow = 360.0f - start_angle_offset_elbow;
         m_curr_theta.shoulder = 360.0f - start_angle_offset_shoulder;
@@ -131,12 +140,15 @@ public:
         m_shoulder_len = shoulder_len;
         m_elbow_len = elbow_len;
     };
-    double get_theta_arm(Point targetXY)
+    double get_theta_shoulder(Point targetXY)
     {
         int quadrant = getQuadrant(targetXY);
-        double angle = atan(targetXY.y / targetXY.x) * 180 / 3.1415; // convert from rad to degrees
-        angle += (90 * (quadrant - 1));
-        return angle;
-    };
+        double angle = Point::to_degrees(targetXY.get_angle());
+        angle += 90;
 
+        if (angle > 360.0f)
+            return angle - 360.0f;
+        else
+            return angle;
+    };
 };
