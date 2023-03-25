689a45f30c20ba303c4e28ab5c5f7a8cf2557b75
pogacee <pogacee@curtisinst.com>
2023-03-25 01:56:27 -0700

    basic locals

--- moveXY.h
+++ moveXY.h
@@ -1,8 +1,16 @@
 #include <iostream>
 
+class moveXY
+{
+private:
+    float m_startX,
+        m_startY,
+        m_start_angle_offset_shoulder,
+        m_start_angle_offset_shoulder;
 
-class moveXY {
-    
-
-
+protected:
+public:
+    moveXY(float startX, float startY, float start_angle_offset_shoulder, float start_angle_offset_elbow) : m_startX{startX}, m_startY{startY}, m_start_angle_offset_shoulder{start_angle_offset_shoulder}, m_start_angle_offset_elbow{start_angle_offset_elbow}
+    {
+    }
 };
