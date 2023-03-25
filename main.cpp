acc6ba2726e51e86759c1f3f2a27d7b745ca8b8e
pogacee <pogacee@curtisinst.com>
2023-03-25 06:08:04 -0700

    more tests

--- main.cpp
+++ main.cpp
@@ -9,10 +9,18 @@ int main()
     constexpr double wristLen{33.4f};
     MoveXY foo(0, 0, armLen, wristLen);
 
-    MoveXY::Point target{-wristLen, -armLen};
+    MoveXY::Point target{16, 16};
     int x = foo.getCircleInts(target);
 
     std::cout << "Num Sol: " << x << std::endl;
-    std::cout << foo.get_theta_shoulder(foo.i1) << ", " << foo.i1.x << " : " << foo.i1.y << std::endl;
-    std::cout << foo.get_theta_shoulder(foo.i2) << ", " << foo.i2.x << " : " << foo.i2.y << std::endl;
-}
+
+    foo.m_curr_elbow_xy.x = 20;
+    foo.m_curr_elbow_xy.y = 20;
+
+    foo.calc_solution_to_target(target);
+
+    std::cout << "sol 1: " << foo.get_theta_shoulder(foo.i1) << ", " << foo.i1.x << " : " << foo.i1.y << " elbow:" << foo.get_theta_elbow(foo.i1, target) << std::endl;
+    std::cout << "sol 2:" << foo.get_theta_shoulder(foo.i2) << ", " << foo.i2.x << " : " << foo.i2.y << " elbow:" << foo.get_theta_elbow(foo.i2, target) << std::endl;
+    std::cout << "pick : " << foo.m_target_theta.shoulder << " elbow:" << foo.m_target_theta.elbow << std::endl;
+    return 1;
+};
--- moveXY.h
+++ moveXY.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -12,13 +13,24 @@ public:
         double x;
         double y;
 
+        /**
+         * @brief Get the magnitude of the vector drawn to this point
+         *
+         * @return double scalar
+         */
         double magnitude()
         {
             return std::sqrt(x * x + y * y);
         }
+
+        /**
+         * @brief Get the angle of the vector drawn to this point
+         *
+         * @return double in degrees
+         */
         double get_angle()
         {
-            return atan2(y, x);
+            return atan2(y, x) * 180 / 3.1415;
         }
 
         void setCoords(double in_x, double in_y)
@@ -26,27 +38,33 @@ public:
             x = in_x;
             y = in_y;
         }
-        static double to_degrees(double radians)
+        void set_mag_angle(double magnitude, double angle)
         {
-            return radians * 180 / 3.1415;
+            y = magnitude * sinf(angle);
+            x = magnitude * cosf(angle);
         }
     };
 
-    struct ArmSolution
+    struct ArmAngles
     {
         double shoulder;
         double elbow;
     };
 
-    Point m_start_xy, m_curr_xy;
-    ArmSolution m_offset_theta;
-    ArmSolution m_curr_theta;
+    Point m_curr_elbow_xy;
+    ArmAngles m_offset_theta, m_curr_theta, m_target_theta;
     float m_elbow_len, m_shoulder_len;
     Point i1, i2;
 
+    /**
+     * @brief Get the Quadrant object
+     *
+     * @param pt the input point
+     * @return int Quadrant that the point is in
+     */
     int getQuadrant(Point pt)
     {
-        int res = -1;
+        int res;
         if (pt.x >= 0)
         {
             res = (pt.y >= 0) ? 1 : 4;
@@ -58,33 +76,81 @@ public:
 
         return res;
     };
-    std::vector<Point> cull_elbow(Point int1, Point int2, int preferredQuadrant)
+
+    /**
+     * @brief Returns tha magnitude of the vector between the target point and the currently stored elbow point
+     *
+     * @param targetPos
+     * @return double scalar
+     */
+    double distanceToElbow(Point targetPos)
+    {
+        return sqrt(((m_curr_elbow_xy.x - targetPos.x) * (m_curr_elbow_xy.x - targetPos.x)) + ((m_curr_elbow_xy.y - targetPos.y) * (m_curr_elbow_xy.y - targetPos.y)));
+    };
+
+    double convertToAdjustedCoordinateSystem(double offset, double start_angle_offset)
+    {
+        return 360 - start_angle_offset + offset;
+    };
+
+    double convertToOriginalCoordinateSystem(double adjustedAngle, double start_angle_offset)
+    {
+        return adjustedAngle + start_angle_offset - 360;
+    };
+
+    /**
+     * @brief Culls non optimal solutions
+     *
+     * @param int1 solution 1
+     * @param int2 solution 2
+     * @param preferredQuadrant preferred quadrant
+     * @return Point that is closest to the current elbow position, in the desired quadrant if possible
+     */
+    Point cull_elbow(Point int1, Point int2, int preferredQuadrant = 0)
     {
         std::vector<Point> outputPoints;
-        if (getQuadrant(int1) == preferredQuadrant)
+        if (preferredQuadrant != 0)
         {
-            outputPoints.push_back(int1);
+            if (getQuadrant(int1) == preferredQuadrant)
+            {
+                outputPoints.push_back(int1);
+            }
+            if (getQuadrant(int2) == preferredQuadrant)
+            {
+                outputPoints.push_back(int2);
+            }
         }
-        if (getQuadrant(int2) == preferredQuadrant)
-        {
-            outputPoints.push_back(int2);
-        }
+
         if (outputPoints.size() == 0)
         {
             outputPoints.push_back(int1);
             outputPoints.push_back(int2);
         }
-        return outputPoints;
-    }
+
+        if (outputPoints.size() == 1)
+        {
+            return outputPoints.at(0);
+        }
+        else if (distanceToElbow(outputPoints.at(0)) <= distanceToElbow(outputPoints.at(1)))
+        {
+            return outputPoints.at(0);
+        }
+        else
+        {
+            return outputPoints.at(1);
+        }
+    };
 
     size_t getCircleInts(Point target)
     {
-
         double d = Point{0 - target.x, 0 - target.y}.magnitude();
 
         // find number of solutions
         if (d > m_shoulder_len + m_elbow_len) // circles are too far apart, no solution(s)
         {
+            m_target_theta.elbow = 180.0f;
+            m_target_theta.shoulder = target.get_angle();
+
             return 0;
         }
         else if (d == 0 && m_shoulder_len == m_elbow_len) // circles coincide
@@ -112,21 +178,54 @@ public:
                 return 1;
             return 2;
         }
+    };
+
+    /**
+     * @brief Get the theta that the shoulder would have to be commanded to reach the desired target.
+     *
+     * @param targetXY
+     * @return double degrees in robot oriented coordinate system
+     */
+    double get_theta_shoulder(Point targetXY)
+    {
+        int quadrant = getQuadrant(targetXY);
+        double angle = targetXY.get_angle() + 90.0f;
+
+        if (angle > 360.0f)
+            return angle - 360.0f;
+        else
+            return angle;
+    };
+
+    /**
+     * @brief Get the theta that the elbow would have to be commanded to reach the desired target given a shoulder position
+     *
+     * @param shoulderXY
+     * @param targetXY
+     * @return double degrees in degrees in robot oriented coordinate system for the elbow
+     */
+    double get_theta_elbow(Point shoulderXY, Point targetXY)
+    {
+        Point delta{targetXY.x - shoulderXY.x, targetXY.y - shoulderXY.y};
+        double res = delta.get_angle() + 90.0f + get_theta_shoulder(shoulderXY);
+        if (res > 360.0f)
+            res -= 360.0f;
+        return res;
     }
 
-    double distanceToElbow(Point targetPos)
+    /**
+     * @brief Calculate the optimal solution to travel to the target XY,
+     * while trying to put the elbow in the desired quadrant. The results are stored in m_target_theta
+     *
+     * @param target target point
+     * @param quadrant requested quadrant for elbow
+     */
+    void calc_solution_to_target(Point target, int quadrant = 1)
     {
-        return sqrt(((m_curr_xy.x - targetPos.x) * (m_curr_xy.x - targetPos.x)) + ((m_curr_xy.y - targetPos.y) * (m_curr_xy.y - targetPos.y)));
-    }
-
-    double convertToAdjustedCoordinateSystem(double offset, double start_angle_offset)
-    {
-        return 360 - start_angle_offset + offset;
-    }
-
-    double convertToOriginalCoordinateSystem(double adjustedAngle, double start_angle_offset)
-    {
-        return adjustedAngle + start_angle_offset - 360;
+        getCircleInts(target);
+        Point res = cull_elbow(i1, i2, quadrant);
+        m_target_theta.shoulder = get_theta_shoulder(res);
+        m_target_theta.elbow = get_theta_elbow(res, target);
     }
 
     MoveXY(float start_angle_offset_shoulder, float start_angle_offset_elbow, float shoulder_len, float elbow_len)
@@ -139,16 +238,5 @@ public:
 
         m_shoulder_len = shoulder_len;
         m_elbow_len = elbow_len;
-    };
-    double get_theta_shoulder(Point targetXY)
-    {
-        int quadrant = getQuadrant(targetXY);
-        double angle = Point::to_degrees(targetXY.get_angle());
-        angle += 90;
-
-        if (angle > 360.0f)
-            return angle - 360.0f;
-        else
-            return angle;
-    };
+    }
 };
