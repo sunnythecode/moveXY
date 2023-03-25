6784f1d9852c033809faee1c1767f870024d7a6a
sunnythecode <45048308+sunnythecode@users.noreply.github.com>
2023-03-25 01:50:43 -0700

    getQuadrant + declaration of cull_elbow

--- moveXY.cpp
+++ moveXY.cpp
@@ -1,1 +1,22 @@
 #include "moveXY.h"
+
+
+int moveXY::getQuadrant(Point pt) {
+    int res = -1;
+    if (pt.x >= 0) 
+    {
+        res = (pt.y >= 0) ? 1:4;
+    } 
+    else 
+    {
+        res = (pt.y>=0) ? 2:3;
+    }
+
+    return res;
+}
+
+std::vector<moveXY::Point> moveXY::cull_elbow() {
+    
+    
+
+}
--- moveXY.h
+++ moveXY.h
@@ -2,7 +2,16 @@
 
 
 class moveXY {
-    
+    public:
+    struct Point {
+        double x;
+        double y;
+    };
+
+    int getQuadrant(Point pt);
+
+    std::vector<Point> cull_elbow();
+
 
 
 };
