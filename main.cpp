2b3508ed98f0f2312c6a7230cc8300da85ffae04
pogacee <pogacee@curtisinst.com>
2023-03-25 02:47:29 -0700

    fixed location

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "moveXY.h"
 #include <iostream>
 
-int main () {
+int main()
+{
     std::cout << "hello world";
-}
 
+    moveXY foo = moveXY(0, 0, 0, 0);
+    moveXY::Point target{4, 5};
+    moveXY::Point p1{0, 0};
+    moveXY::Point p2{0, 0};
+    int x = foo.getCircleInts(5, 6, target, p1, p2);
+
+    std::cout << "Num Sol" << x << std::endl;
+    std::cout << p1.x << ", " << p1.y;
+    std::cout << p2.x << ", " << p2.y;
+}
